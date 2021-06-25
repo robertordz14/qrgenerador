1abe6cb8b2b9ec3a9cce776bb7c92c5c6111f0a5
robertordz14 <hroberto.rdz@gmail.com>
2021-06-25 11:39:40 -0500

    qr5

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  // Link
-} from "react-router-dom";
-
-import Home from './pages/Home'
-import QRgen from './pages/QRgenerator'
-import QRscan from './pages/QRscanner'
+import React, {useState} from 'react'
+import {Fab, TextField, Grid} from '@material-ui/core'
+import {GetApp} from '@material-ui/icons'
+import QRcode from 'qrcode.react';
 
 function App() {
+  const [qr, setQr] = useState('https://enrutate.vercel.app/maps/');
+  const handleChange = (event) => {
+      setQr(event.target.value);
+  };
+  const descargarQR = () => {
+      const canvas = document.getElementById("qrEnrutate");
+      const pngUrl = canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = "qrEnrutate.png";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+  };
+
   return (
-    <div className="App">
-      <div className="App-header">
-      
-        <Router>
-          <div>
-
-            <Switch>
-              <Route exact path="/">
-                <Home/>
-              </Route>
-              <Route path="/qr_generator">
-                <QRgen/>
-              </Route>
-              <Route path="/qr_scanner">
-                <QRscan/>
-              </Route>
-            </Switch>
-
+    <div className="generador">
+   
+          <h3>Generador de QR Enrútate</h3>
+          
+          <div style={{marginTop:30}}>
+              <TextField onChange={handleChange} style={{width:320}}
+              value={qr} label="Liga de la parada" size="small" variant="outlined"  
+              />
           </div>
-        </Router>
 
-      </div>
+          <div>
+              {
+                  qr ?
+                  <QRcode 
+                      id="qrEnrutate"
+                      value={qr} 
+                      size={420}
+                      includeMargin={true}
+                  /> :
+                  <p>No existen datos para generar QR</p>
+              }
+          </div>
+          <div id="descarga">
+              {
+                  qr ? 
+                  <Grid container>
+                  
+                      <Grid item xs={2}>
+                      <Fab onClick={descargarQR} style={{marginLeft:10}} color="primary">
+                          <GetApp/>
+                      </Fab>
+                      </Grid>
+                  </Grid> :
+                  ''
+              }
+          </div>
     </div>
   );
 }
